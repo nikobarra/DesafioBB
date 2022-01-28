8cd0bdf7867b9a81ce167ffe4c18656bd4824c51
Nicolas <nicolasbarrapelecano@gmail.com>
2022-01-28 11:06:23 -0300

    functional click to full movies

--- Desafio.py
+++ Desafio.py
@@ -1,36 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-
-
 
 options = Options()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])#con estas options evito que de error usb y bluetooth
 
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-
-website = 'https://www.starz.com/ar/es/movies'
+website = 'https://www.starz.com/ar/es/'
 path = 'C:\driver\chromedriver'
 driver = webdriver.Chrome(path,chrome_options=options)
-
+driver.implicitly_wait(20) #un tipo de timer para que de tiempo de carga a la pagina y encuentre los elementos clickeables ya renderizados en el dom
 driver.get(website)
 
-try:
-    element = WebDriverWait (driver,50).until(EC.presence_of_element_located((By.ID, "subview-container")))
-    xpath='//*[@id="subview-container"]/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div[1]/a/div'
+xpath='//*[@id="application-wrapper"]/starz-sidebar/div/div/ul/li[3]/a'#path click peliculas
 
-    xpath2="//section[@id='subview-container']/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div/a/div"
-    cssSlect="//section[@id='subview-container']/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div/a/div"
+xpath2='//*[@id="view-container"]/starz-header/header/div/div/div[4]/button/span[1]'#path del menu hamburguesa 
 
-    see_all_movies= driver.find_element_by_xpath(xpath)
-    
+xpath3= '//*[@id="subview-container"]/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div[1]/a/div' #path ver todas
+
+def click_movies(xpath):
+    see_all_movies= driver.find_element_by_xpath(xpath) 
     see_all_movies.click()
-finally:
-    driver.quit()
 
 
-
+click_movies(xpath2)
+click_movies(xpath)
+click_movies(xpath3)
 
 
