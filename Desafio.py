feba433b7385f355d9bef9393c7ad76fd6faed34
Nicolas <nicolasbarrapelecano@gmail.com>
2022-01-28 10:16:20 -0300

    agregado try para que cargue y tampoco anda!

--- Desafio.py
+++ Desafio.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+
+
 
 options = Options()
 
@@ -13,9 +16,19 @@ path = 'C:\driver\chromedriver'
 driver = webdriver.Chrome(path,chrome_options=options)
 
 driver.get(website)
-see_all_movies= driver.find_element_by_css_selector('css=starz-dynamic-container:nth-child(2) .view-all > .text-uppercase')
+
+try:
+    element = WebDriverWait (driver,50).until(EC.presence_of_element_located((By.ID, "subview-container")))
+    xpath='//*[@id="subview-container"]/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div[1]/a/div'
+
+    xpath2="//section[@id='subview-container']/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div/a/div"
+    cssSlect="//section[@id='subview-container']/starz-movies/div/starz-block-page-render/div/starz-dynamic-container[2]/div/div/div/starz-std-slider/div/div/a/div"
+
+    see_all_movies= driver.find_element_by_xpath(xpath)
     
-see_all_movies.click()
+    see_all_movies.click()
+finally:
+    driver.quit()
 
 
 
